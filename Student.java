36311310b3d23342063c9e09133c90ba765eb663
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-11-22 18:16:31 +0530

    getter and setter

--- Student.java
+++ Student.java
@@ -24,60 +24,42 @@ public class Student {
         super();
     }
 
-    /**
-     * @return int return the studentId
-     */
     public int getStudentId() {
         return studentId;
     }
 
-    /**
-     * @param studentId the studentId to set
-     */
     public void setStudentId(int studentId) {
         this.studentId = studentId;
     }
 
-    /**
-     * @return String return the studentName
-     */
     public String getStudentName() {
         return studentName;
     }
 
-    /**
-     * @param studentName the studentName to set
-     */
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
-    /**
-     * @return String return the studentCity
-     */
     public String getStudentCity() {
         return studentCity;
     }
 
-    /**
-     * @param studentCity the studentCity to set
-     */
     public void setStudentCity(String studentCity) {
         this.studentCity = studentCity;
     }
 
-    /**
-     * @return String return the studentPhone
-     */
     public String getStudentPhone() {
         return studentPhone;
     }
 
-    /**
-     * @param studentPhone the studentPhone to set
-     */
     public void setStudentPhone(String studentPhone) {
         this.studentPhone = studentPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " studentId='" + getStudentId() + "'" + ", studentName='" + getStudentName() + "'"
+                + ", studentCity='" + getStudentCity() + "'" + ", studentPhone='" + getStudentPhone() + "'" + "}";
     }
 
 }
